552fecd85ea358bcbb31ffd0e0ae9cbe465eb351
ANISH7550 <anishkumar90602@gmail.com>
2025-10-23 19:39:29 +0530

    Add files via upload

---
+++ SRTF.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Process {
+    int pid;
+    int arrivalTime;
+    int burstTime;
+    int remainingTime;
+    int waitingTime;
+    int turnaroundTime;
+};
+
+int main() {
+    int n;
+    cout << "=== SRTF Scheduling Algorithm (Preemptive SJF) ===\n";
+    cout << "Enter number of processes: ";
+    cin >> n;
+
+    vector<Process> procs(n);
+    for(int i = 0; i < n; i++) {
+        procs[i].pid = i + 1;
+        cout << "Enter Arrival Time for Process " << i + 1 << ": ";
+        cin >> procs[i].arrivalTime;
+        cout << "Enter Burst Time for Process " << i + 1 << ": ";
+        cin >> procs[i].burstTime;
+        procs[i].remainingTime = procs[i].burstTime;
+    }
+
+    vector<bool> done(n, false);
+    int completed = 0, currentTime = 0;
+    double totalWT = 0, totalTAT = 0;
+
+    cout << "\\nPID  AT  BT  WT  TAT\\n";
+
+    while(completed < n) {
+        int idx = -1;
+        int minRem = 1000000000;
+        for(int i = 0; i < n; i++) {
+            if(!done[i] && procs[i].arrivalTime <= currentTime && procs[i].remainingTime < minRem && procs[i].remainingTime > 0) {
+                minRem = procs[i].remainingTime;
+                idx = i;
+            }
+        }
+
+        if(idx == -1) { currentTime++; continue; }
+
+        procs[idx].remainingTime--;
+        currentTime++;
+
+        if(procs[idx].remainingTime == 0) {
+            procs[idx].turnaroundTime = currentTime - procs[idx].arrivalTime;
+            procs[idx].waitingTime = procs[idx].turnaroundTime - procs[idx].burstTime;
+
+            totalWT += procs[idx].waitingTime;
+            totalTAT += procs[idx].turnaroundTime;
+
+            done[idx] = true;
+            completed++;
+
+            cout << procs[idx].pid << "    "
+                 << procs[idx].arrivalTime << "    "
+                 << procs[idx].burstTime << "    "
+                 << procs[idx].waitingTime << "    "
+                 << procs[idx].turnaroundTime << "\\n";
+        }
+    }
+
+    cout.setf(ios::fixed); cout<<setprecision(2);
+    cout << "\\nAverage Waiting Time: " << totalWT / n << "\\n";
+    cout << "Average Turnaround Time: " << totalTAT / n << "\\n";
+
+    return 0;
+}
