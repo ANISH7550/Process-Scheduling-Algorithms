771e8f3456c747e89d9d3d2a5bf4536ec1184f68
ANISH7550 <anishkumar90602@gmail.com>
2025-10-23 19:41:03 +0530

    Add files via upload

---
+++ PBR.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Process {
+    int pid;
+    int arrivalTime;
+    int burstTime;
+    int priority;
+    int waitingTime;
+    int turnaroundTime;
+};
+
+int main() {
+    int n;
+    cout << "=== Priority Scheduling Algorithm (Non-preemptive) ===\n";
+    cout << "Enter number of processes: ";
+    cin >> n;
+
+    vector<Process> procs(n);
+    for(int i = 0; i < n; i++) {
+        procs[i].pid = i + 1;
+        cout << "Enter Arrival Time for Process " << i + 1 << ": ";
+        cin >> procs[i].arrivalTime;
+        cout << "Enter Burst Time for Process " << i + 1 << ": ";
+        cin >> procs[i].burstTime;
+        cout << "Enter Priority (lower number = higher) for Process " << i + 1 << ": ";
+        cin >> procs[i].priority;
+    }
+
+    vector<bool> done(n, false);
+    int completed = 0, currentTime = 0;
+    double totalWT = 0, totalTAT = 0;
+
+    cout << "\\nPID  AT  BT  Priority  WT  TAT\\n";
+
+    while(completed < n) {
+        int idx = -1;
+        int highestPrio = 1000000000;
+        for(int i = 0; i < n; i++) {
+            if(!done[i] && procs[i].arrivalTime <= currentTime && procs[i].priority < highestPrio) {
+                highestPrio = procs[i].priority;
+                idx = i;
+            }
+        }
+        if(idx == -1) { currentTime++; continue; }
+
+        procs[idx].waitingTime = currentTime - procs[idx].arrivalTime;
+        currentTime += procs[idx].burstTime;
+        procs[idx].turnaroundTime = procs[idx].waitingTime + procs[idx].burstTime;
+
+        totalWT += procs[idx].waitingTime;
+        totalTAT += procs[idx].turnaroundTime;
+
+        done[idx] = true;
+        completed++;
+
+        cout << procs[idx].pid << "    "
+             << procs[idx].arrivalTime << "    "
+             << procs[idx].burstTime << "    "
+             << procs[idx].priority << "         "
+             << procs[idx].waitingTime << "    "
+             << procs[idx].turnaroundTime << "\\n";
+    }
+
+    cout.setf(ios::fixed); cout<<setprecision(2);
+    cout << "\\nAverage Waiting Time: " << totalWT / n << "\\n";
+    cout << "Average Turnaround Time: " << totalTAT / n << "\\n";
+
+    return 0;
+}
